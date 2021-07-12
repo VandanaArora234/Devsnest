2d583281d7313417dd833cd83268f1e67732881c
Vandana <aroravandana234@gmail.com>
2021-07-13 00:01:27 +0530

    overWrite tha 16

--- tha-16/src/App.jsx
+++ tha-16/src/App.jsx
@@ -10,6 +10,7 @@ function App()
         <div className="app">
         <Card />
         <Board />
+        
         </div>
     );
 }
--- tha-16/src/components/Board.jsx
+++ tha-16/src/components/Board.jsx
@@ -1,109 +1,39 @@
 
+function Cell(){
+    return(
+        <div className="col"></div>
+    );
+}
+function Row(){
+    return(
+
+    <div className="row">
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+    </div>
+
+    );
+}
+
 
 function Board()
 {
     return(
         <div className="chess-board">
-
-          <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          </div>
-
-          <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          </div>
-
-          <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          </div>
-
-          <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          </div>
-
-          <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          </div>
-
-          <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          </div>
-
-          <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          </div>
-
-          <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          </div>
-
-          <div className="row">
-               <div className="col"></div>
-               <div className="col"></div>
-               <div className="col"></div>
-               <div className="col"></div>
-               <div className="col"></div>
-               <div className="col"></div>
-               <div className="col"></div>
-               <div className="col"></div>
-          </div>
-
+         <Row />
+         <Row />
+         <Row />
+         <Row />
+         <Row />
+         <Row />
+         <Row />
+         <Row />
         </div>
     );
 }
