353dc6b6e15a368cca525c2ba6b6a838501b4bc6
Vandana <aroravandana234@gmail.com>
2021-10-16 22:57:39 +0530

    tha-16 backend

--- Backend/tha16/app.js
+++ Backend/tha16/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter =  require('./routes/index');
 const streamRouter = require('./routes/stream')
 
 var app = express();
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+      res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
